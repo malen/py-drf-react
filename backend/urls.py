ee0a507c361e55649903cef4e2552a937e15b331
malen <malen.ma@gmail.com>
2026-04-04 22:36:56 +0900

    配置Swagger

--- backend/urls.py
+++ backend/urls.py
@@ -19,9 +19,26 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+# Swagger
+from rest_framework import permissions, routers
 
 from api.views import HelloWorldView, LoginView, UserViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="DRF + React API",
+        default_version="v1",
+        description="API documentation for my project",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 router = routers.DefaultRouter()
 router.register(r"", UserViewSet)
@@ -36,6 +53,13 @@ urlpatterns = [
     path("api/login/", LoginView.as_view(), name="login"),
     # 包含 books 应用的 URL
     path("books/", include("api.books.urls")),  # 包含 books 应用的
+    # Swagger 文档
+    path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 
 # +++++++++++++++++++++++++++++++++++++++++++++++
